acc0cc50319b6cc322c6d41ced1331f854d0862c
Eridho Buffery Rollian <eridhobffry@yahoo.com>
2021-09-18 11:52:33 +0200

    fonticon remove and add

--- src/application/components/building_block/radio_details_preview/component.js
+++ src/application/components/building_block/radio_details_preview/component.js
@@ -10,11 +10,11 @@ const RadioDetailsPreview = (props) => {
             {
                 isSelected && selectedRadio === radioName &&
                 <Styled.RadioImageWrapper>
-                    <FontIcon color={theme.color.color15} icon='add_circle_outline' borderless />
+                    <FontIcon color={theme.color.color15} icon='remove_circle_outline' borderless />
                     <ImagePlaceholder
                         source={radioImage}
                     />
-                    <FontIcon color={theme.color.color15} icon='remove_circle_outline' borderless />
+                    <FontIcon color={theme.color.color15} icon='add_circle_outline' borderless />
                 </Styled.RadioImageWrapper>
             }
             <RadioList onRadioClick={selectRadio} {...props} />
